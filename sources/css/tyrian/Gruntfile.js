17d35928d66f9e4810a0f51292f883394320df2e
Alex Legler <alex@a3li.li>
2015-01-05 20:47:34 +0100

    Rename CSS portion to gentoo-tyrian-css, adapt Grunt functionality

--- sources/css/tyrian/Gruntfile.js
+++ sources/css/tyrian/Gruntfile.js
@@ -40,36 +40,14 @@ module.exports = function(grunt) {
                 ]
             }
         },
-        copy: {
-            update_assets: {
-                files: [
-                    { expand: true, cwd: '../bootstrap/dist/css/', src: ['bootstrap.css', 'bootstrap.min.css'], dest: '../../../assets/css/'},
-                    { expand: true, cwd: '../bootstrap/dist/fonts/', src: '*', dest: '../../../assets/fonts/'},
-                    { expand: true, cwd: '../bootstrap/dist/js/', src: '*', dest: '../../../assets/js/'},
-                    { expand: true, cwd: 'dist/', src: 'tyrian*.css', dest: '../../../assets/css/'},
-                    { expand: true, cwd: 'fonts/', src: '*', dest: '../../../assets/fonts/'},
-                ]
-            }
-        },
         sed: {
             inject_variables: {
                 path: '../bootstrap/less/bootstrap.less',
-                pattern: '@import "variables.less";',
+                pattern: /@import "variables\.less";$/m,
                 replacement: '@import "variables.less"; @import "../../tyrian/bootstrap/variables-tyrian.less";'
             }
         },
         shell: {
-            update_bootstrap: {
-                command: [
-                    'git checkout -- .',
-                    'git pull --rebase'
-                ].join('&&'),
-                options: {
-                    execOptions: {
-                        cwd: '../bootstrap/'
-                    }
-                }
-            },
             build_bootstrap: {
                 command: 'grunt dist',
                 options: {
@@ -82,7 +60,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-replace");
     grunt.loadNpmTasks("grunt-sed");
@@ -94,15 +71,14 @@ module.exports = function(grunt) {
     // Compresses tyrian sources
     grunt.registerTask("compress", ["less:minify"]);
 
-    // Updates assets in the main directory
-    grunt.registerTask("update_assets", ["copy:update_assets"]);
-
     // Updates bootstrap, injects our custom variables, and builds bootstrap
-    grunt.registerTask("bootstrap", ["shell:update_bootstrap", "sed:inject_variables", "shell:build_bootstrap"]);
+    grunt.registerTask("bootstrap", [ "sed:inject_variables", "shell:build_bootstrap"]);
 
     // by default: compile and compress
-    grunt.registerTask("default", ["compile", "compress"]);
+    grunt.registerTask("dist", ["compile", "compress"]);
 
-    grunt.registerTask("all", ["bootstrap", "compile", "compress", "update_assets"]);
+    grunt.registerTask("default", ["compile"]);
+
+    grunt.registerTask("all", ["bootstrap", "compile", "compress"]);
 };
 
